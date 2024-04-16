5688d2867ed51d9561b14fba430bf3c7dc3fb169
Nyan <nyandude22@gmail.com>
2024-04-16 13:07:02 +0200

    Updated guide and CSS

--- src/controllers/useMongoDBGuide.js
+++ src/controllers/useMongoDBGuide.js
@@ -6,12 +6,13 @@ export const useMongoDBGuide = () => {
   useEffect(() => {
     setContent([
       {
-        title: 'Introduksjon til MongoDB',
-        description: 'MongoDB er en NoSQL-database som tilbyr høy ytelse, høy tilgjengelighet, og enkel skalering.',
-      },
+        "title": "Introduksjon til MongoDB",
+        "description": "MongoDB er en NoSQL-database som tilbyr høy ytelse, høy tilgjengelighet, og enkel skalering. Den er ideell for webapplikasjoner som krever rask databehandling og fleksibel datahåndtering, slik som dynamiske blogging-plattformer eller sosiale medier apper hvor innholdet stadig endres og oppdateres.",
+        "images": ["/imgs/MongoDB-Logo.png"]
+      },      
       {
         title: 'Forutsetninger',
-        description: 'Før du installerer MongoDB, sørg for at systemet ditt møter disse forutsetningene.',
+        description: 'Før du installerer MongoDB, sørg for at du gjør dette først.',
         images: ["/imgs/step1.png", "/imgs/step2.png"],
         codes: [
           { code: 'sudo apt update', description: 'Oppdater den lokale pakkedatabasen.' },
@@ -33,7 +34,9 @@ export const useMongoDBGuide = () => {
         codes: [
           { code: 'sudo apt-get install -y mongodb-org', description: 'Installer alle MongoDB-pakkene' },
           { code: 'sudo systemctl start mongod', description: 'Start MongoDB' },
-          { code: 'sudo systemctl status mongod', description: 'Sjekk status for MongoDB, se etter "active"' }
+          { code: 'sudo systemctl status mongod', description: 'Sjekk status for MongoDB, se etter "active"' },
+          { code: 'mongosh', description: 'Åpne MongoDB Shellet' }
+
         ]
       },
       {
@@ -43,7 +46,39 @@ export const useMongoDBGuide = () => {
           { code: 'sudo systemctl enable mongod', description: 'Aktiver automatisk oppstart' }
         ]
       },
-
+      {
+        title: 'Sikre din MongoDB-server',
+        description: 'Konfigurer sikkerhetstiltak og opprett en administratorbruker for å administrere databasen.',
+        codes: [
+          { code: 'mongosh', description: 'Start MongoDB shell' },
+          { code: 'use admin', description: 'Bytt til admin-databasen' },
+          { code: `db.createUser({
+            user: "admin",
+            pwd: passwordPrompt(),
+            roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
+          })`, description: 'Opprett en administratorbruker med tilgang til alle databaser' }
+        ]
+      },
+      {
+        title: 'Aktivere autentisering i MongoDB',
+        description: 'For å sikre at kun autoriserte brukere har tilgang til databasene, må autentisering aktiveres i MongoDBs konfigurasjonsfil.',
+        codes: [
+          { 
+            code: `security:\n  authorization: "enabled"`, 
+            description: 'Legg til eller endre denne linjen under security-seksjonen i mongod.conf for å aktivere autentisering.' 
+          }
+        ]
+      },
+      {
+        title: 'Koble til MongoDB fra din applikasjon',
+        description: 'Nå som MongoDB er satt opp og sikret, kan du koble til databasen din fra applikasjonen ved å bruke en tilkoblingsstreng. Denne strengen vil avhenge av din applikasjons miljø og de bibliotekene du bruker.',
+        codes: [
+          {
+            code: `const mongoose = require('mongoose');\nmongoose.connect('mongodb://admin:yourSecurePassword@yourServerIP:27017/admin', {\n    useNewUrlParser: true,\n    useUnifiedTopology: true\n});`,
+            description: 'Erstatt "yourServerIP" med IP-adressen eller vertsnavnet til din MongoDB-server, og tilpass brukernavn og passord som du har konfigurert.'
+          }
+        ]
+      }
     ]);
   }, []);
 
--- src/views/Guide.js
+++ src/views/Guide.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMongoDBGuide } from '../controllers/useMongoDBGuide';
 import '../styles/Guide.css';
 
 const Guide = () => {
   const { content } = useMongoDBGuide();
   const [copySuccess, setCopySuccess] = useState({ show: false, message: '', index: -1 });
-  const [modalImage, setModalImage] = useState(null); // State to hold the modal image
+  const [modalImage, setModalImage] = useState(null);
+
+  const handleScrollToSection = (index) => {
+    const section = document.getElementById(`section${index}`);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
 
   const handleCopyCode = (code, sectionIndex, codeIndex) => {
-    const uniqueId = `${sectionIndex}-${codeIndex}`; // Create a unique ID for each code block
+    const uniqueId = `${sectionIndex}-${codeIndex}`;
     navigator.clipboard.writeText(code).then(() => {
-        setCopySuccess({ show: true, message: 'Koden er kopiert til utklippstavlen!', index: uniqueId });
+      setCopySuccess({ show: true, message: 'Koden er kopiert til utklippstavlen!', index: uniqueId });
       setTimeout(() => setCopySuccess({ show: false, message: '', index: '' }), 2000);
     }, (err) => {
       console.error('Could not copy text:', err);
@@ -27,13 +34,23 @@ const Guide = () => {
     setModalImage(null);
   };
 
-  
-
   return (
     <div className="container">
+      <div className="sidebar">
+        {content.map((section, index) => (
+          <button
+            key={index}
+            className={`sidebar-item ${modalImage && modalImage === `section${index}` ? 'active' : ''}`}
+            onClick={() => handleScrollToSection(index)}
+          >
+            {section.title}
+          </button>
+        ))}
+      </div>
+
       <h1>MongoDB Guide</h1>
       {content.map((section, sectionIndex) => (
-        <div key={sectionIndex} className="section">
+        <div key={sectionIndex} className="section" id={`section${sectionIndex}`}>
           <h2>{section.title}</h2>
           <p>{section.description}</p>
           {section.images && section.images.map((img, imgIndex) => (
@@ -65,7 +82,7 @@ const Guide = () => {
           <span className="close" onClick={closeModal}>&times;</span>
           <img className="modal-content" src={modalImage} alt="Modal Content" />
         </div>
-            )}
+      )}
     </div>
   );
 };
